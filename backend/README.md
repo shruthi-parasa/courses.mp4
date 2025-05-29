72975bf1f68f03b4fd9033f9fe3b78f50610b100
Loc <locnguyen110816@gmail.com>
2025-05-29 00:59:29 -0700

    Youtube API call

---
+++ backend/README.md
@@ -0,0 +1,32 @@
+Create Virtual Environment:
+```bash
+python -m venv venv/
+```
+
+Get inside Virtual Environment:
+
+- Windows(cmd):
+```bash
+venv\Scripts\activate
+```
+
+- Mac:
+```bash
+source venv/bin/activate
+```
+
+Go to Backend folder:
+```bash
+cd backend
+```
+
+Install Python packages
+```bash
+pip install -r requirements.txt
+```
+
+Update requirements.txt
+```bash
+pip freeze > requirements.txt
+```
+
---
+++ backend/youtube/youtube.py
@@ -0,0 +1,48 @@
+from googleapiclient.discovery import build
+from dotenv import load_dotenv
+import os
+import json
+
+# Youtube API key
+load_dotenv()
+youtube_key = os.getenv("YOUTUBE_API")
+
+def fetch_videos(query, video_num):
+  try:
+    # Fetch videos from Youtube
+    youtube = build("youtube", "v3", developerKey=youtube_key)
+    request = youtube.search().list(
+        part = "snippet",
+        q = query,
+        type = "video",
+        maxResults = video_num
+    )
+    response = request.execute()
+
+    # Organize videos
+    result = []
+    for item in response["items"]:
+      
+      id = item["id"]["videoId"]
+      
+      video = {
+        "title": item["snippet"]["title"],
+        "id": id,
+        "channel": item["snippet"]["channelTitle"],
+        "thumbnail": item["snippet"]["thumbnails"]["default"]["url"],
+        "publish_time": item["snippet"]["publishedAt"],
+        "video_url": f"https://www.youtube.com/watch?v={id}"
+      }
+      
+      json_video = json.dumps(video)
+      result.append(json_video)
+      
+    return result
+  except Exception as e:
+    print("Error fetching video:", e)
+    return []
+
+if __name__ == "__main__":
+  videos = fetch_videos("Svelte tutorial", 5)
+  for vid in videos:
+    print(vid)
